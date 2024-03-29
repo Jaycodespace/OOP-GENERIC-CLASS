886a6d2fd87b2d73b95a94931cc59c016e5992cb
jaycodespace <121559050+Jaycodespace@users.noreply.github.com>
2024-03-29 22:25:24 +0800

    Update Arithmetic.java

--- Arithmetic/Arithmetic.java
+++ Arithmetic/Arithmetic.java
@@ -1,1 +1,40 @@
+package MyMap;
 
+import java.util.ArrayList;
+
+class MyMap<Key, Value> {
+    private final ArrayList<Key> keys;
+    private final ArrayList<Value> values;
+
+    public MyMap() {
+        keys = new ArrayList<>();
+        values = new ArrayList<>();
+    }
+
+    public void put(Key key, Value value) {
+        if (keys.contains(key)) {
+            int index = keys.indexOf(key);
+            values.set(index, value);
+        } else {
+            keys.add(key);
+            values.add(value);
+        }
+    }
+
+    public Value get(Key key) {
+        if (keys.contains(key)) {
+            int index = keys.indexOf(key);
+            return values.get(index);
+        }
+        return null;
+    }
+
+    public Value remove(Key key) {
+        if (keys.contains(key)) {
+            int index = keys.indexOf(key);
+            keys.remove(index);
+            return values.remove(index);
+        }
+        return null;
+    }
+}
