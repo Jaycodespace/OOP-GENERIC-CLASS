85833832308092cd9faa61ec4016e7d2d43e2312
jaycodespace <121559050+Jaycodespace@users.noreply.github.com>
2024-03-29 22:29:56 +0800

    Update MyMap.java

--- MyMap/MyMap.java
+++ MyMap/MyMap.java
@@ -1,42 +1,40 @@
 package MyMap;
 
-import java.util.Scanner;
+import java.util.ArrayList;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+class MyMap<Key, Value> {
+    private final ArrayList<Key> keys;
+    private final ArrayList<Value> values;
 
+    public MyMap() {
+        keys = new ArrayList<>();
+        values = new ArrayList<>();
+    }
 
-        System.out.print("Enter a (string) key to put: ");
-        String string_to_put = scan.nextLine();
-        System.out.print("Enter an (integer) value to put: ");
-        int integer_to_put = scan.nextInt();
-        scan.nextLine();
-
-        MyMap<Object, Object> myMap = new MyMap<>();
-        myMap.put(string_to_put, integer_to_put);
-
-        System.out.print("Enter a (string) key to search: ");
-        String key_to_search = scan.nextLine();
-
-        Integer value_to_get = (Integer) myMap.get(key_to_search);
-
-        if (value_to_get != null) {
-            System.out.println("Value for key '" + key_to_search + "': " + value_to_get);
+    public void put(Key key, Value value) {
+        if (keys.contains(key)) {
+            int index = keys.indexOf(key);
+            values.set(index, value);
         } else {
-            throw new IllegalArgumentException("No value found for key '" + key_to_search + "'");
-        }
-
-        System.out.print("Enter a (string) key to remove: ");
-        String key_to_remove = scan.nextLine();
-
-        Integer value_to_remove = (Integer) myMap.remove(key_to_remove);
-
-        if (value_to_remove != null) {
-            System.out.println("Removed value for key '" + key_to_remove + "': " + value_to_remove);
-        } else {
-            throw new IllegalArgumentException("Key '" + key_to_remove + "' not found in the map");
+            keys.add(key);
+            values.add(value);
         }
     }
-}
 
+    public Value get(Key key) {
+        if (keys.contains(key)) {
+            int index = keys.indexOf(key);
+            return values.get(index);
+        }
+        return null;
+    }
+
+    public Value remove(Key key) {
+        if (keys.contains(key)) {
+            int index = keys.indexOf(key);
+            keys.remove(index);
+            return values.remove(index);
+        }
+        return null;
+    }
+}
